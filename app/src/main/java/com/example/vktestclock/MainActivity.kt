f9afccd9dcb81b752f1f69e0432d418baec78214
Lineked <lineked@yandex.ru>
2024-03-19 21:44:30 +0300

    some changes

--- app/src/main/java/com/example/vktestclock/MainActivity.kt
+++ app/src/main/java/com/example/vktestclock/MainActivity.kt
@@ -2,17 +2,12 @@ package com.example.vktestclock
 
 import android.graphics.Color
 import android.os.Bundle
-import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ImageView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.vktestclock.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private var _binding: ActivityMainBinding? = null
+    private var f: Boolean = false
     private val binding
         get() = _binding ?: throw IllegalStateException("Binding for ActivityMainBinding must not be null")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +15,13 @@ class MainActivity : AppCompatActivity() {
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.button.setOnClickListener {
-            binding.customClockView.setNumberColor(Color.BLUE)
+            f = if (f) {
+                binding.customClockView1.setNumberColor(Color.BLUE)
+                false
+            } else {
+                binding.customClockView1.setNumberColor(Color.GREEN)
+                true
+            }
         }
     }
 }
